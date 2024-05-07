9bdf9fd361cc27f5d74227f566ab2474d464889f
RPin3 <113200552+RPin3@users.noreply.github.com>
2024-05-07 16:10:23 -0700

    olatilin
    ijuesuesotilin777

--- JS/app.js
+++ JS/app.js
@@ -12,3 +12,32 @@ btnCalcular.addEventListener('click', function () {
         let imc = peso / (altura * altura);
         document.getElementById('resultado').value = imc.toFixed(1);
 });
+
+
+//practicaII
+
+const btnCalcularp2 = document.getElementById('btnCalcularp2');
+const btnLimpiarp2 = document.getElementById('btnLimpiarp2');
+
+btnCalcularp2.addEventListener('click', function calcular(){
+const opcion = document.getElementById('idOpcion').value;
+const idNumero1 = document.getElementById('idNumero1').value;
+const idNumero2 = document.getElementById('idNumero2').value;
+const txtres = document.getElementById('idResultado');
+
+let res = 0;
+let num1=parseInt(idNumero1);
+let num2 = parseInt(idNumero2);
+let opc = parseInt(opcion);
+
+switch(opc){
+case 1: res = num1+num2; break;
+case 2: res = num1-num2; break;
+case 3: res = num1*num2; break;
+case 4: res = num1/num2; break;
+}
+
+txtres.value=res;
+
+});
+
